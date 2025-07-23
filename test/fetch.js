a5155cc5997d614954151609662710514fe7f163
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-07-23 11:22:28 +0500

    add test fetch file

--- test/fetch.js
+++ test/fetch.js
@@ -1,10 +1,30 @@
+import CurlImpersonate from "node-curl-impersonate";
+
 async function scrape(params) {
-  const res = await fetch(process.env.SMART_DATA_TRADING_URL + "/alerts", {
-    redirect: "manual",
-    headers: { "user-agent": process.env.USER_AGENT },
-  });
-  console.log(await res.text());
-  console.log(res);
+  const headers = { "user-agent": process.env.USER_AGENT };
+  //   const res = await fetch(process.env.SMART_DATA_TRADING_URL + "/alerts", {
+  //     redirect: "manual",
+  //     headers: { "user-agent": process.env.USER_AGENT },
+  //   });
+  //   console.log(await res.text());
+  //   console.log(res);
+
+  const curlImpersonate = new CurlImpersonate(
+    process.env.SMART_DATA_TRADING_URL + "/alerts",
+    {
+      method: "GET",
+      headers: headers,
+      followRedirects: false,
+    }
+  );
+
+  const curlResponse = await curlImpersonate.makeRequest();
+
+  // extract the response data
+  const response = curlResponse.response;
+  const responseStatusCode = curlResponse.statusCode;
+  console.log(curlResponse);
+  console.log(responseStatusCode);
 }
 
 scrape();
