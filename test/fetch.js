20563528c15c77998512d7c7d7e8517e97758fd8
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-07-23 11:20:48 +0500

    add test fetch file

--- test/fetch.js
+++ test/fetch.js
@@ -1,6 +1,7 @@
 async function scrape(params) {
   const res = await fetch(process.env.SMART_DATA_TRADING_URL + "/alerts", {
     redirect: "manual",
+    headers: { "user-agent": process.env.USER_AGENT },
   });
   console.log(await res.text());
   console.log(res);
